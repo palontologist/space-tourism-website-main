e328c8bb9e0722b38f4fa041febd3860fd210b32
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-08 09:57:58 +0000

    Add shadow effect to the button when hovering

--- vite-project/src/pages/Home/Home.tsx
+++ vite-project/src/pages/Home/Home.tsx
@@ -1,22 +1,29 @@
+import { Link } from "react-router-dom";
+import Menu from "../../Menu";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-        <div className={styles.description}>
-          <p className={styles.heading}>SO, YOU WANT TO TRAVEL TO</p>
-          <h1 className={styles.title}>SPACE</h1>
-          <p className={styles.info}>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
+    <>
+      <Menu />
+      <div className={styles.background}>
+        <div className={styles.container}>
+          <div className={styles.description}>
+            <p className={styles.heading}>SO, YOU WANT TO TRAVEL TO</p>
+            <h1 className={styles.title}>SPACE</h1>
+            <p className={styles.info}>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <Link to="destination">
+            <button className={styles.button}>EXPLORE</button>
+          </Link>
         </div>
-        <button className={styles.button}>EXPLORE</button>
       </div>
-    </div>
+    </>
   );
 };
 
