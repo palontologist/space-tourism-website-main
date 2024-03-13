6a5c10f7e7448c412880b6ee990762441099116f
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-13 14:45:43 +0000

    Fix routes

--- vite-project/src/pages/Destination/Destination.tsx
+++ vite-project/src/pages/Destination/Destination.tsx
@@ -8,39 +8,43 @@ const Destination = () => {
       <Menu />
       <div className={styles.layout}>
         <div>
-          <h1>
-            <span>01</span>PICK YOUR DESTINATION
-          </h1>
-        </div>
-        <div className={styles[`planets-menu`]}>
-          <nav>
-            <NavLink to="moon">
-              {({ isActive }) => (
-                <p className={isActive ? styles.active : ""}>MOON</p>
-              )}
-            </NavLink>
-          </nav>
-          <nav>
-            <NavLink to="mars">
-              {({ isActive }) => (
-                <p className={isActive ? styles.active : ""}>MARS</p>
-              )}
-            </NavLink>
-          </nav>
-          <nav>
-            <NavLink to="europa">
-              {({ isActive }) => (
-                <p className={isActive ? styles.active : ""}>EUROPA</p>
-              )}
-            </NavLink>
-          </nav>
-          <nav>
-            <NavLink to="titan">
-              {({ isActive }) => (
-                <p className={isActive ? styles.active : ""}>TITAN</p>
-              )}
-            </NavLink>
-          </nav>
+          <div>
+            <h1>
+              <span>01</span>PICK YOUR DESTINATION
+            </h1>
+          </div>
+          <div>
+            <div className={styles[`planets-menu`]}>
+              <nav>
+                <NavLink to="../moon">
+                  {({ isActive }) => (
+                    <p className={isActive ? styles.active : ""}>MOON</p>
+                  )}
+                </NavLink>
+              </nav>
+              <nav>
+                <NavLink to="../mars">
+                  {({ isActive }) => (
+                    <p className={isActive ? styles.active : ""}>MARS</p>
+                  )}
+                </NavLink>
+              </nav>
+              <nav>
+                <NavLink to="../europa">
+                  {({ isActive }) => (
+                    <p className={isActive ? styles.active : ""}>EUROPA</p>
+                  )}
+                </NavLink>
+              </nav>
+              <nav>
+                <NavLink to="../titan">
+                  {({ isActive }) => (
+                    <p className={isActive ? styles.active : ""}>TITAN</p>
+                  )}
+                </NavLink>
+              </nav>
+            </div>
+          </div>
         </div>
       </div>
     </div>
