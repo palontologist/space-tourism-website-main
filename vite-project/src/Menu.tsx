5159698c6c26bb3d6c114159d8ca9f04fc8b844e
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-21 14:49:04 +0000

    Fix Menu styling

--- vite-project/src/Menu.tsx
+++ vite-project/src/Menu.tsx
@@ -4,55 +4,57 @@ import styles from "./Menu.module.css";
 const Menu = () => {
   return (
     <div className={styles.menu}>
-      <svg
-        className={styles.logo}
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-      >
-        <g fill="none" fill-rule="evenodd">
-          <circle cx="24" cy="24" r="24" fill="#FFF" />
-          <path
-            fill="#0B0D17"
-            d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-          />
-        </g>
-      </svg>
-      <div className={styles.line}></div>
+      <div className={styles.logoContainer}>
+        <svg
+          className={styles.logo}
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+        >
+          <g fill="none" fill-rule="evenodd">
+            <circle cx="24" cy="24" r="24" fill="#FFF" />
+            <path
+              fill="#0B0D17"
+              d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+            />
+          </g>
+        </svg>
+      </div>
+      {/* <div className={styles.line}></div> */}
       <ul className={styles[`menu-list`]}>
         <li>
           <NavLink to="/">
             {({ isActive }) => (
-              <p className={isActive ? styles.active : ""}>
+              <span className={isActive ? styles.active : ""}>
                 <span className={styles.bold}>00</span> HOME
-              </p>
+              </span>
             )}
           </NavLink>
         </li>
         <li>
           <NavLink to="/destination/moon">
             {({ isActive }) => (
-              <p className={isActive ? styles.active : styles[`not-moon`]}>
+              <span className={isActive ? styles.active : styles[`not-moon`]}>
                 <span className={styles.bold}>01</span> DESTINATION
-              </p>
+              </span>
             )}
           </NavLink>
         </li>
         <li>
           <NavLink to="/crew/commander">
             {({ isActive }) => (
-              <p className={isActive ? styles.active : ""}>
+              <span className={isActive ? styles.active : ""}>
                 <span className={styles.bold}>02</span> CREW
-              </p>
+              </span>
             )}
           </NavLink>
         </li>
         <li>
           <NavLink to="/technology">
             {({ isActive }) => (
-              <p className={isActive ? styles.active : ""}>
+              <span className={isActive ? styles.active : ""}>
                 <span className={styles.bold}>03</span> TECHNOLOGY
-              </p>
+              </span>
             )}
           </NavLink>
         </li>
