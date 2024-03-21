acd6d74368e28b1838425774506a114090943393
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-21 14:48:46 +0000

    Fix Home styling

--- vite-project/src/pages/Home/Home.tsx
+++ vite-project/src/pages/Home/Home.tsx
@@ -6,22 +6,20 @@ const Home = () => {
   return (
     <>
       <Menu />
-      <div className={styles.background}>
-        <div className={styles.container}>
-          <div className={styles.description}>
-            <p className={styles.heading}>SO, YOU WANT TO TRAVEL TO</p>
-            <h1 className={styles.title}>SPACE</h1>
-            <p className={styles.info}>
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
-          <Link to="destination/moon">
-            <button className={styles.button}>EXPLORE</button>
-          </Link>
+      <div className={styles.container}>
+        <div className={styles.description}>
+          <p className={styles.heading}>SO, YOU WANT TO TRAVEL TO</p>
+          <h1 className={styles.title}>SPACE</h1>
+          <p className={styles.info}>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
         </div>
+        <Link to="destination/moon">
+          <button className={styles.button}>EXPLORE</button>
+        </Link>
       </div>
     </>
   );
