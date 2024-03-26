2c4af84d4b3c1d79f92e9009913641437d5d8939
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-26 15:40:46 +0000

    complete crew design

--- vite-project/src/pages/Crew/Crew.tsx
+++ vite-project/src/pages/Crew/Crew.tsx
@@ -1,36 +1,69 @@
 import { NavLink } from "react-router-dom";
-import Menu from "../../Menu";
+import Menu from "../../components/Menu/Menu";
 import styles from "./Crew.module.css";
-import Member from "../Member/Member";
+import MemberData from "../../components/Member/Member";
+import data from "../../data.json";
 
 interface Props {
   crew: string;
 }
 
 const Crew = ({ crew }: Props) => {
+  const crewData = data.crew.find((member) => member.role === crew);
+
   return (
     <div className={styles.background}>
       <Menu />
-      <div>
-        <div>
-          <h1 className={styles.title}>
-            <span>02</span>MEET YOUR CREW
-          </h1>
-        </div>
+
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          <span>02</span>MEET YOUR CREW
+        </h1>
 
         <div className={styles[`second-section`]}>
-          <div className={styles[`crew-menu`]}>
-            <nav>
-              <NavLink to="../commander">
-                {({ isActive }) => (
-                  <div
-                    className={`${isActive ? styles.active : styles.option} `}
-                  ></div>
-                )}
-              </NavLink>
-            </nav>
+          <div className={styles[`main-content`]}>
+            <ul className={styles[`crew-menu`]}>
+              <li>
+                <NavLink to="../commander">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    ></div>
+                  )}
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="../mission-specialist">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    ></div>
+                  )}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="../pilot">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    ></div>
+                  )}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="../flight-engineer">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    ></div>
+                  )}
+                </NavLink>
+              </li>
+            </ul>
+            <MemberData crew={crew} />
           </div>
-          <Member crew={crew} />
+          <img className={styles.photo} src={crewData?.images.png} />
         </div>
       </div>
     </div>
