259d3e3a44c28558ca5a31ec253dbbaa0a13acad
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-27 15:37:14 +0000

    Make Technology page responsive for tablet screen

--- vite-project/src/pages/Technology/Technology.tsx
+++ vite-project/src/pages/Technology/Technology.tsx
@@ -3,15 +3,35 @@ import Menu from "../../components/Menu/Menu";
 import styles from "./Technology.module.css";
 import data from "../../data.json";
 import TechnologyData from "../../components/TechnologyData/TechnologyData";
+import { useEffect, useState } from "react";
 
 interface Props {
   technology: string;
 }
 
 const Technology = ({ technology }: Props) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
+
   const technologyData = data.technology.find(
     (member) => member.name === technology
   );
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 1280) {
+        setIsLargeScreen(true);
+      }
+      if (window.innerWidth < 1280) {
+        setIsLargeScreen(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className={styles.background}>
@@ -23,44 +43,53 @@ const Technology = ({ technology }: Props) => {
         </h1>
 
         <div className={styles[`second-section`]}>
-          <ul className={styles[`technology-menu`]}>
-            <li>
-              <NavLink to="../launch-vehicle">
-                {({ isActive }) => (
-                  <div
-                    className={`${isActive ? styles.active : styles.option} `}
-                  >
-                    1
-                  </div>
-                )}
-              </NavLink>
-            </li>
+          <div className={styles[`main-body`]}>
+            <ul className={styles[`technology-menu`]}>
+              <li>
+                <NavLink to="../launch-vehicle">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    >
+                      1
+                    </div>
+                  )}
+                </NavLink>
+              </li>
 
-            <li>
-              <NavLink to="../spaceport">
-                {({ isActive }) => (
-                  <div
-                    className={`${isActive ? styles.active : styles.option} `}
-                  >
-                    2
-                  </div>
-                )}
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="../space-capsule">
-                {({ isActive }) => (
-                  <div
-                    className={`${isActive ? styles.active : styles.option} `}
-                  >
-                    3
-                  </div>
-                )}
-              </NavLink>
-            </li>
-          </ul>
-          <TechnologyData technology={technology} />
-          <img className={styles.photo} src={technologyData?.images.portrait} />
+              <li>
+                <NavLink to="../spaceport">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    >
+                      2
+                    </div>
+                  )}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="../space-capsule">
+                  {({ isActive }) => (
+                    <div
+                      className={`${isActive ? styles.active : styles.option} `}
+                    >
+                      3
+                    </div>
+                  )}
+                </NavLink>
+              </li>
+            </ul>
+            <TechnologyData technology={technology} />
+          </div>
+          <img
+            className={styles.photo}
+            src={
+              isLargeScreen
+                ? technologyData?.images.portrait
+                : technologyData?.images.landscape
+            }
+          />
         </div>
       </div>
     </div>
