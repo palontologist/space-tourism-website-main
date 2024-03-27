7f9fb8c23b6552eb6383ea6dac8a0c7554249b99
Jessy Yeh <jiejun.yeh@gmail.com>
2024-03-27 11:05:37 +0000

    create a wrapper for menu and remove position fixed

--- vite-project/src/components/Menu/Menu.tsx
+++ vite-project/src/components/Menu/Menu.tsx
@@ -3,62 +3,64 @@ import styles from "./Menu.module.css";
 
 const Menu = () => {
   return (
-    <div className={styles.menu}>
-      <div className={styles.logoContainer}>
-        <svg
-          className={styles.logo}
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-        >
-          <g fill="none" fill-rule="evenodd">
-            <circle cx="24" cy="24" r="24" fill="#FFF" />
-            <path
-              fill="#0B0D17"
-              d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-            />
-          </g>
-        </svg>
+    <div className={styles.menuWrapper}>
+      <div className={styles.menu}>
+        <div className={styles.logoContainer}>
+          <svg
+            className={styles.logo}
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <circle cx="24" cy="24" r="24" fill="#FFF" />
+              <path
+                fill="#0B0D17"
+                d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+              />
+            </g>
+          </svg>
+        </div>
+        {/* <div className={styles.line}></div> */}
+        <ul className={styles[`menu-list`]}>
+          <li>
+            <NavLink to="/">
+              {({ isActive }) => (
+                <span className={isActive ? styles.active : ""}>
+                  <span className={styles.bold}>00</span> HOME
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/destination/moon">
+              {({ isActive }) => (
+                <span className={isActive ? styles.active : styles[`not-moon`]}>
+                  <span className={styles.bold}>01</span> DESTINATION
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/crew/commander">
+              {({ isActive }) => (
+                <span className={isActive ? styles.active : ""}>
+                  <span className={styles.bold}>02</span> CREW
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/technology/launch-vehicle">
+              {({ isActive }) => (
+                <span className={isActive ? styles.active : ""}>
+                  <span className={styles.bold}>03</span> TECHNOLOGY
+                </span>
+              )}
+            </NavLink>
+          </li>
+        </ul>
       </div>
-      {/* <div className={styles.line}></div> */}
-      <ul className={styles[`menu-list`]}>
-        <li>
-          <NavLink to="/">
-            {({ isActive }) => (
-              <span className={isActive ? styles.active : ""}>
-                <span className={styles.bold}>00</span> HOME
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/destination/moon">
-            {({ isActive }) => (
-              <span className={isActive ? styles.active : styles[`not-moon`]}>
-                <span className={styles.bold}>01</span> DESTINATION
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/crew/commander">
-            {({ isActive }) => (
-              <span className={isActive ? styles.active : ""}>
-                <span className={styles.bold}>02</span> CREW
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/technology/launch-vehicle">
-            {({ isActive }) => (
-              <span className={isActive ? styles.active : ""}>
-                <span className={styles.bold}>03</span> TECHNOLOGY
-              </span>
-            )}
-          </NavLink>
-        </li>
-      </ul>
     </div>
   );
 };
